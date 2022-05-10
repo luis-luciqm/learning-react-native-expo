852a7be99a17eb0a65447f239456c0d3fd6d331d
Luís Lucilândio <48826657+luis-luciqm@users.noreply.github.com>
2022-05-10 16:23:50 -0300

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View style={styles.bottoms}>
+          <Text>Menu</Text>
+        </View>
+        <View style={styles.bottoms}>
+          <Text>Logs</Text>
+        </View>
+        <View style={styles.bottoms}>
+          <Text>Credits</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,7 +23,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    
   },
+
+  header: {
+    backgroundColor: '#f2f2f2',
+    // width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  
+
 });
